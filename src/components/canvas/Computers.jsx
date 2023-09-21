dd5cdd47956889c96a3d70a588c042af1afd0271
Brandonnn3 <106995522+Brandonnn3@users.noreply.github.com>
2023-09-21 19:46:19 -0400

    Changed GLTF Model to disable on mobile view

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -42,25 +42,25 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
+
+  // If it's a mobile device, render nothing or some placeholder content.
+  if (isMobile) {
+    return null; 
+    // or return some other component or placeholder if you wish.
+    // e.g. return <MobilePlaceholder />;
+  }
 
   return (
     <Canvas
@@ -79,7 +79,6 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );
